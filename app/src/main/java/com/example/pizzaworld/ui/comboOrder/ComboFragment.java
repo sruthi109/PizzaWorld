d332556d7b5f3d1e0f5c93549202e3d892c723eb
tejamalempati <tejamalempati23@gmail.com>
2020-11-29 18:06:49 -0500

    Combo Fragment added

--- app/src/main/java/com/example/pizzaworld/ui/comboOrder/ComboFragment.java
+++ app/src/main/java/com/example/pizzaworld/ui/comboOrder/ComboFragment.java
@@ -1,5 +1,6 @@
 package com.example.pizzaworld.ui.comboOrder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,59 +8,145 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.pizzaworld.Details;
 import com.example.pizzaworld.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ComboFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ComboFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ComboFragment extends Fragment {
+
+    ImageView chickenbites,wingit,pizzawithfries,largepizzawithdrinks,friesnuggets,pizzawithdippings;
+    TextView chickenbitesname,wingitname,pizzawithfriesname,largepizzawithdrinksname,friesnuggetsname,pizzawithdippingsname;
+    TextView chickenbitesprice,wingitprice,pizzawithfriesprice,largepizzawithdrinksprice,friesnuggetsprice,pizzawithdippingsprice;
 
     public ComboFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComboFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ComboFragment newInstance(String param1, String param2) {
-        ComboFragment fragment = new ComboFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_combo, container, false);
+        View root=  inflater.inflate(R.layout.fragment_combo, container, false);
+        chickenbitesprice=root.findViewById(R.id.pricechickenbites);
+        wingitprice=root.findViewById(R.id.pricewingit);
+        pizzawithfriesprice=root.findViewById(R.id.pricepizzawithfries);
+        largepizzawithdrinksprice=root.findViewById(R.id.pricelargepizzawithdrinks);
+        friesnuggetsprice=root.findViewById(R.id.pricefriesnuggets);
+        pizzawithdippingsprice=root.findViewById(R.id.pizzawithdippingsprice);
+
+
+        chickenbites=root.findViewById(R.id.chickenbites);
+        wingit=root.findViewById(R.id.wingit);
+        pizzawithfries=root.findViewById(R.id.pizzawithfries);
+        largepizzawithdrinks=root.findViewById(R.id.largepizzawithdrinks);
+        friesnuggets=root.findViewById(R.id.friesnuggets);
+        pizzawithdippings=root.findViewById(R.id.pizzawithdippings);
+
+
+
+        chickenbitesname=root.findViewById(R.id.chickenbitesname);
+        wingitname=root.findViewById(R.id.wingitname);
+        pizzawithfriesname=root.findViewById(R.id.pizzawithfriesname);
+        largepizzawithdrinksname=root.findViewById(R.id.largepizzawithdrinksname);
+        friesnuggetsname=root.findViewById(R.id.friesnuggetsname);
+        pizzawithdippingsname=root.findViewById(R.id.pizzawithdippingsname);
+
+        chickenbites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=chickenbitesprice.getText().toString();
+                String name=chickenbitesname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.chicken_bites);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+
+        wingit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=wingitprice.getText().toString();
+                String name=wingitname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.wingit);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+        pizzawithfries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=pizzawithfriesprice.getText().toString();
+                String name=pizzawithfriesname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.pizzawithfires);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+
+        largepizzawithdrinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=largepizzawithdrinksprice.getText().toString();
+                String name=largepizzawithdrinksname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.largepizzawithdrinks);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+
+
+        friesnuggets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=friesnuggetsprice.getText().toString();
+                String name=friesnuggetsname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.friesnuggets);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+
+        pizzawithdippings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=pizzawithdippingsprice.getText().toString();
+                String name=pizzawithdippingsname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.pizzawithdippings);
+                intent.putExtra("name",name);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+        return root;
     }
 }
