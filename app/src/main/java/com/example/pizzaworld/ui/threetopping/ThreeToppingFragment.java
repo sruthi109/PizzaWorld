9f1c847a400f84dbcdee4842a6d61b23b10ddc4f
sumanth2091156 <sumanth.gaddam090@gmail.com>
2020-12-01 01:21:59 -0500

    updating ThreeToppingFragment file UI

--- app/src/main/java/com/example/pizzaworld/ui/threetopping/ThreeToppingFragment.java
+++ app/src/main/java/com/example/pizzaworld/ui/threetopping/ThreeToppingFragment.java
@@ -1,5 +1,6 @@
 package com.example.pizzaworld.ui.threetopping;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,59 +8,137 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.pizzaworld.Details;
 import com.example.pizzaworld.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ThreeToppingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ThreeToppingFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+public class ThreeToppingFragment extends Fragment {
+
+    // LinearLayout firstlayout;
+    ImageView pepperoniPizza,supremepizza,cheesepizza,workpizza,verdugopizza,mellowmushroompizza;
+    TextView pepperoniprice,pricesupreme,cheeseprice,workprice,verdugopizzaprice,mellowmushroompizzaprice;
+    TextView pepperoniname,supremename,cheesename,workname,verdugopizzaname,mellowmushroompizzaname;
+
+
 
     public ThreeToppingFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThreeToppingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ThreeToppingFragment newInstance(String param1, String param2) {
-        ThreeToppingFragment fragment = new ThreeToppingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three_topping, container, false);
+        View root= inflater.inflate(R.layout.fragment_three_topping, container, false);
+
+        // firstlayout=root.findViewById(R.id.firstllayout);
+        pepperoniprice=root.findViewById(R.id.pricepepporni);
+        pricesupreme=root.findViewById(R.id.pricesupreme);
+        cheeseprice=root.findViewById(R.id.pricecheese);
+        workprice=root.findViewById(R.id.pricework);
+        verdugopizzaprice=root.findViewById(R.id.priceverdugopizza);
+        mellowmushroompizzaprice=root.findViewById(R.id.mellowmushroompizzaprice);
+
+
+        pepperoniPizza=root.findViewById(R.id.pepporonipizza);
+        supremepizza=root.findViewById(R.id.supremepizza);
+        cheesepizza=root.findViewById(R.id.cheesepizza);
+        workpizza=root.findViewById(R.id.workpizza);
+        verdugopizza=root.findViewById(R.id.verdugopizza);
+        mellowmushroompizza=root.findViewById(R.id.mellowmushroompizza);
+
+        pepperoniname=root.findViewById(R.id.pepporoniName);
+        supremename=root.findViewById(R.id.supremename);
+        cheesename=root.findViewById(R.id.cheesename);
+        workname=root.findViewById(R.id.workname);
+        verdugopizzaname=root.findViewById(R.id.verdugopizzaname);
+        mellowmushroompizzaname=root.findViewById(R.id.mellowmushroompizzaname);
+
+
+        pepperoniPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=pepperoniprice.getText().toString();
+                String name=pepperoniname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.pepporonipizza);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+        workpizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=workprice.getText().toString();
+                String name=workname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.workpizza);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+        cheesepizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=cheeseprice.getText().toString();
+                String name=cheesename.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.cheesepizza);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+        supremepizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=pricesupreme.getText().toString();
+                String name=supremename.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.supremepizza);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+
+        verdugopizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=verdugopizzaprice.getText().toString();
+                String name=verdugopizzaname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.verdugopizza);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+
+        mellowmushroompizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String price=mellowmushroompizzaprice.getText().toString();
+                String name=mellowmushroompizzaname.getText().toString();
+                Intent intent = new Intent(getActivity(), Details.class);
+                intent.putExtra("price",price);
+                intent.putExtra("image",R.drawable.mellowmushroom);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+
+        return root;
     }
+
+
 }
