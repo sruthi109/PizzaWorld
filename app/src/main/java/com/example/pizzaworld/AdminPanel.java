d960ed2236e91c6342713181d35e1e73b2bd1070
Sravani1320 <nagasravani9@gmail.com>
2020-12-19 12:48:47 +0530

    added admin panel xml

--- app/src/main/java/com/example/pizzaworld/AdminPanel.java
+++ app/src/main/java/com/example/pizzaworld/AdminPanel.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class AdminPanel extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
