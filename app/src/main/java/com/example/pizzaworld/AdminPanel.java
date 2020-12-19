829e37199fc7db147e1b6888bd845ee2554864c6
Sravani1320 <Sravani1320>
2020-12-19 12:35:18 +0530

    Admin Panel xml is added

--- app/src/main/java/com/example/pizzaworld/AdminPanel.java
+++ app/src/main/java/com/example/pizzaworld/AdminPanel.java
@@ -10,5 +10,7 @@ public class AdminPanel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
+
+
     }
 }
