fd2cb91174ff703b213507d7701940a346ef892e
Sravani1320 <nagasravani9@gmail.com>
2020-12-21 10:05:22 +0530

    added anim java code to splash

--- app/src/main/java/com/example/pizzaworld/Splash.java
+++ app/src/main/java/com/example/pizzaworld/Splash.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
-
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);
+        imageView=findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.spin);
+        imageView.startAnimation(animation);
+
         Thread timer=new Thread()
         {
             public void run() {
                 try {
-                    sleep(2000);
+                    sleep(5000);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
